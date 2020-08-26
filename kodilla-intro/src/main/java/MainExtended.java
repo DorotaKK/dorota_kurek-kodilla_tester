c52f772be5e21c84d830d02894f87a7d7fb0877f
Dorota Kurek <kurekdorota@gmail.com>
2020-08-26 21:38:15 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myNickname = "Kura";
+        int myNumber = 11;
+        double pi = 3.14;
+        char mercedesClass = 'E';
+        String mySport = "ski";
+        boolean isGoodSkier = true;
+        System.out.println(myNickname);
+        System.out.println(myNumber);
+        System.out.println(pi);
+        System.out.println(mercedesClass);
+        System.out.println(mySport);
+        System.out.println(isGoodSkier);
+    }
+}
